8976e2fbf47100ec02b1b6eef3c02e784240e6c8
jessica you <jessicaxytan@gmail.com>
2021-08-19 14:37:57 -0400

    commit tree 5ad0bfaf4b42caa4c416c5efd01d37051d43cfb0

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
+require 'pry'
 
+class CashRegister
+    attr_accessor :total, :discount, :price, :items
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+    end
+
+    def add_item(title, price, quantity)
+        if quantity == 1
+         self.total = price * quantity + self.total
+        end
+    end
+
+end
